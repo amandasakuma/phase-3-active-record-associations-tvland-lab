25e47deffb3c60b80b588b14834784f29726b30f
Amanda Sakuma <amandasakuma@gmail.com>
2022-09-05 14:04:32 -0400

    commit tree 963de58f1b488bf37dd69360d2b67e5a85e94b1c

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    belongs_to :network
+    has_many :actors, through: :characters
+
+    def actors_list
+        self.actors.map do |actor|
+            "#{actor.first_name} #{actor.last_name}"
+            #actor.full_name also works bc it was defined in actor.rb
+        end
+    end
 end
